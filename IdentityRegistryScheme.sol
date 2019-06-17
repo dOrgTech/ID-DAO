828aab55bea7a70d23be10aa1f73329b76fa5f74
Luis Dominguez <ld@luis.sh>
2019-06-17 02:21:05 -0400

    Adding scheme pseudocode, along with .gitignore

---
+++ IdentityRegistryScheme.sol
@@ -0,0 +1,17 @@
+/*
+
+Psuedocode:
+
+
+contract IdentityRegistryScheme is UniversalScheme, VotingMachineCallbacks, ProposalExecuteInterface {
+
+  event AddIdentityProposal
+  event RemoveIdentityProposal
+  event UpdateIdentityProposal
+
+  
+
+
+}
+
+*/
