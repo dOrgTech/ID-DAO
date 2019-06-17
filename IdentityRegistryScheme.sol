0ebb0225655bc4bb5c5e1ad7b29af91260061151
Luis Dominguez <ld@luis.sh>
2019-06-17 02:39:51 -0400

    Fleshing out events, added proposal struct, identityProposal mapping, and basic get function

--- IdentityRegistryScheme.sol
+++ IdentityRegistryScheme.sol
@@ -3,14 +3,34 @@
 Psuedocode:
 
 
+import './IdentityRegistryInterface.sol'
+
 contract IdentityRegistryScheme is UniversalScheme, VotingMachineCallbacks, ProposalExecuteInterface {
 
-  event AddIdentityProposal
-  event RemoveIdentityProposal
-  event UpdateIdentityProposal
+  enum ProposalType { ADD, REMOVE, UPDATE }
+  address identityRegistryAddress;
 
-  
+  event AddIdentityProposal(bytes32 indexed _proposalID, address _addressID, string _name, bool _isRegistered, string _metadataURI, bytes _metadata)
+  event RemoveIdentityProposal(bytes32 indexed _proposalID, address _addressID, string _name, bool _isRegistered, string _metadataURI, bytes _metadata))
+  event UpdateIdentityProposal(indexed _proposalID, address _addressID)
 
+  event ProposalExecuted(bytes32 indexed _proposalID, ProposalType _proposalType)
+  event ProposalDeleted(bytes32 indexed _proposalID, ProposalType _proposalType)
+
+  struct IdentityProposal {
+    bytes32 proposalID;
+    ProposalType proposalType;
+    bytes32 values; //Encoded values of proposal, as is different depending on function    
+  }
+
+  // Mapping the address of proposer to proposals
+  // In this paradigm, we store sets of proposal IDs per proposer should this be simply ID => IdentityProposal, keeping proposer in struct?
+  mapping(address => mapping(bytes32 => IdentityProposal)) public identityProposals;
+
+  function getProposal(address proposer, bytes32 proposalID) public view returns (ProposalType, bytes32){
+    IdentityProposal memory proposal = identityProposals[proposer][proposalID];
+    return (proposal.proposalType, proposal.values);
+  }
 
 }
 
