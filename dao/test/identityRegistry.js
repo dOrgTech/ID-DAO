6f2992ddf38c5cfed757cff7adc83e8eb2e50623
Luis Dominguez <ld@luis.sh>
2019-07-22 14:00:21 -0400

    Broke out revert function into own class

--- dao/test/identityRegistry.js
+++ dao/test/identityRegistry.js
@@ -6,6 +6,7 @@
 'use strict';
 
 const common = require('./common');
+const solAssert = require('./solAssert');
 const util = require('util');
 
 let instances = {};
@@ -21,7 +22,7 @@ contract('Testing IdentityRegistry', (accounts) => {
 
   describe('Only owner can...', async () => {
     it('add', async () => {
-      await assertSolidityRevert(
+      await solAssert.revert(
         async () => {
           await instances.IdentityRegistry.add(users[0], '0x0', { from: users[0] });
         }
@@ -29,7 +30,7 @@ contract('Testing IdentityRegistry', (accounts) => {
     })
 
     it('remove', async () => {
-      await assertSolidityRevert(
+      await solAssert.revert(
         async () => {
           await instances.IdentityRegistry.remove(users[0], { from: users[0] });
         }
@@ -37,7 +38,7 @@ contract('Testing IdentityRegistry', (accounts) => {
     })
 
     it('update', async () => {
-      await assertSolidityRevert(
+      await solAssert.revert(
         async () => {
           await instances.IdentityRegistry.update(users[0], '0x0', { from: users[0] });
         }
@@ -59,25 +60,4 @@ contract('Testing IdentityRegistry', (accounts) => {
 
 
 
-
-
-// TODO: Move into seperate file: 
-
-  //Simply function to test for Solidity revert errors; optionally takes an "expectedErr" which simply looks for a string within
-  //This function has limits, however; if a function can potentially return two or more reverts, we can't quite test for each of them through expectedErr and apply if/and/or logic
-  async function assertSolidityRevert(run, expectedErr = null) {
-    let err;
-    try {
-      await run();
-    } catch(_e) {
-      err = _e.message;
-    }
-    //Ensure we've received an error
-    assert.ok(err, 'No error from function call');
-    assert.isTrue(err.includes('VM Exception while processing transaction: revert'), 'Error received: ' + err);
-    console.log(err)
-    if(expectedErr != null) assert.isTrue(err.includes(expectedErr));
-    
-    return err;
-  }
 
