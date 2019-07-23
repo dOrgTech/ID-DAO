d3e8ec291fd37bdb949c8307dd417abd98081e6d
Luis Dominguez <ld@luis.sh>
2019-07-22 20:01:40 -0400

    Fleshing out IdentityRegistry tests, along with isHuman()

--- dao/test/IdentityRegistry.js
+++ dao/test/IdentityRegistry.js
@@ -14,7 +14,22 @@ let instances = {};
 contract('Testing IdentityRegistry', (accounts) => {
 
   const owner = accounts[0];
-  const users = accounts.slice(1);
+  const users = [
+    {
+       address: accounts[1],
+       metadata: '0xb5689b817f4d6dc4d28676ea1a0af6bdeac27ceaf5381cfe'
+    },
+    {   
+       address: accounts[2],
+       metadata: '0x0589a7eda839a6d2e0af0d7a4af3c3e69b2b2473b69daf77'
+    },
+    {
+       address: accounts[3],
+       metadata: '0x2964d850f3c1249264908cf530b787a5bd83229a57948233'
+    }
+  ];
+
+
 
   it('Deploy IdentityRegistry', async () => {
     instances.IdentityRegistry = await contracts.IdentityRegistry.new({ from: owner });
@@ -24,7 +39,7 @@ contract('Testing IdentityRegistry', (accounts) => {
     it('add', async () => {
       await solAssert.revert(
         async () => {
-          await instances.IdentityRegistry.add(users[0], '0x0', { from: users[0] });
+          await instances.IdentityRegistry.add(users[0].address, '0x0', { from: users[0].address });
         }
       , 'Ownable: caller is not the owner');
     })
@@ -32,7 +47,7 @@ contract('Testing IdentityRegistry', (accounts) => {
     it('remove', async () => {
       await solAssert.revert(
         async () => {
-          await instances.IdentityRegistry.remove(users[0], { from: users[0] });
+          await instances.IdentityRegistry.remove(users[0].address, { from: users[0].address });
         }
       , 'Ownable: caller is not the owner');
     })
@@ -40,10 +55,36 @@ contract('Testing IdentityRegistry', (accounts) => {
     it('update', async () => {
       await solAssert.revert(
         async () => {
-          await instances.IdentityRegistry.update(users[0], '0x0', { from: users[0] });
+          await instances.IdentityRegistry.update(users[0].address, '0x0', { from: users[0].address });
         }
       , 'Ownable: caller is not the owner');
     })
+  })
+
+  describe('isHuman()', async () => {
+
+    it('user is not human before registration', async () => {
+      let res = await instances.Registry.isHuman.call(users[0].address);
+      assert.isFalse(res);
+    })
+    
+    it('add ID to registry', async () => {
+      //Add an ID to the Registry
+      let res = await instances.Registry.add(users[0].address, users[0].metadata, { from: owner });
+      assert.ok(res);
+
+      //Check if added
+      let metadata = await instances.Registry.registry.call(users[0].address);
+      assert.ok(metadata, 'error, metadata is: ' + metadata);
+      assert.equal(metadata, users[0].metadata, 'metadata returned not expected'); 
+    })
+
+    it('', async () => {
+      
+      
+
+    })
+
   })
 
 })
