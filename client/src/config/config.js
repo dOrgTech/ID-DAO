837cd96e8eb4231b3dff5111c8b6083e43e9c4bd
Luis Dominguez <ld@luis.sh>
2019-07-29 03:31:52 -0400

    Correction of config interface, writing out IdentityRegistry class

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,10 @@
 interface Config {
-  idRegistryAddress: string;
+  IdentityRegistry: IdentityRegistryConfig;
+}
+
+interface IdentityRegistryConfig {
+  address: string;
+  abi: object;
 }
 
 export = Config;
--- client/src/registry/identity-registry.ts
+++ client/src/registry/identity-registry.ts
@@ -1,11 +1,16 @@
 import web3 = require('web3');
+import Config = require('../config/Config');
 
 class IdentityRegistry {
 
   web3: web3;
+  config: Config;
+  contract: Contract;
 
-  constructor(web3: web3){
-    this.web3 = web3
+  constructor(web3: web3, config: Config){
+    this.web3 = web3;
+    this.config = config;
+    this.contract = new web3.eth.Contract(config.IdentityRegistry.address);
   }
 
   
