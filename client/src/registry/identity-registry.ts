d11bef8d0caa6d711a57d25f8e5004d93c5da1c6
Luis Dominguez <ld@luis.sh>
2019-07-29 03:47:01 -0400

    Adding contract interface functions

--- client/src/registry/identity-registry.ts
+++ client/src/registry/identity-registry.ts
@@ -7,12 +7,25 @@ class IdentityRegistry {
   config: Config;
   contract: Contract;
 
-  constructor(web3: web3, config: Config){
+  constructor(web3: web3, config: Config) {
     this.web3 = web3;
     this.config = config;
     this.contract = new web3.eth.Contract(config.IdentityRegistry.address);
   }
 
+  async add(id: string, metadata: string): object {
+    //NOTE: Assumption is defaultAccount for now
+    return await this.contract.methods.add(id, metadata).send({ from: web3.eth.defaultAccount });
+  }
+
+  async remove(id: string): object {
+    return await this.contract.method.remove(id).send({ from: web3.eth.defaultAccount });    
+  }
+  
+
+  async isHuman(id: string): boolean {
+    return await this.contract.methods.isHuman(id).call();
+  }
   
 
 }
