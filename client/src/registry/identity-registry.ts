937daaabeaba617d8fab936d9731356821fcfc6e
Luis Dominguez <ld@luis.sh>
2019-07-29 04:21:48 -0400

    Promise compilation error cleared

--- client/src/registry/identity-registry.ts
+++ client/src/registry/identity-registry.ts
@@ -1,5 +1,5 @@
 import Web3 = require('web3');
-import Config = require('../config/Config');
+import Config = require('../config/config');
 
 class IdentityRegistry {
 
@@ -13,25 +13,25 @@ class IdentityRegistry {
     this.contract = new web3.eth.Contract(config.IdentityRegistry.address);
   }
 
-  async add(id: string, metadata: string): object {
+  async add(id: string, metadata: string): Promise<object> {
     //NOTE: Assumption is defaultAccount for now
     return await this.contract.methods.add(id, metadata).send({ from: this.web3.eth.defaultAccount });
   }
 
-  async remove(id: string): object {
+  async remove(id: string): Promise<object> {
     return await this.contract.method.remove(id).send({ from: this.web3.eth.defaultAccount });    
   }
   
-  async update(id: string, metadata: string): object {
+  async update(id: string, metadata: string): Promise<object> {
     return await this.contract.method.update(id, metadata).send({ from: this.web3.eth.defaultAccount });
   }
 
-  async removeSelf(sender: string=this.web3.eth.defaultAccount): object {
+  async removeSelf(sender: string=this.web3.eth.defaultAccount): Promise<object> {
     return await this.contract.method.removeSelf().send({ from: sender });
 
   }
 
-  async isHuman(id: string): boolean {
+  async isHuman(id: string): Promise<boolean> {
     return await this.contract.methods.isHuman(id).call();
   }
   
