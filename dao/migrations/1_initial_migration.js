9c8d1dd647edcd3f95ea67e027150d65a3cd7efb
Luis Dominguez <ld@luis.sh>
2019-07-31 19:22:52 -0400

    Added IdentityRegistry migration, and fixed up development network settings. All scripts, both wihin client and dao, are functioning

--- dao/migrations/1_initial_migration.js
+++ dao/migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
+const IdentityRegistry = artifacts.require("IdentityRegistry");
+
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+
+  //First, deploy Migrations contract
   deployer.deploy(Migrations);
+
+  //Deploy IdentityRegistry contract
+  deployer.deploy(IdentityRegistry);
+
 };
