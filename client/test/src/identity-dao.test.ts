c732f4bd4320089e4ea6eca159969a0d93b1a346
Luis Dominguez <ld@luis.sh>
2019-07-31 19:35:53 -0400

    Installed truffle-hdwallet-provider, a config file with the constant mnemonnic seed we'll be using, and fleshing out test initial code

--- client/test/src/identity-dao.test.ts
+++ client/test/src/identity-dao.test.ts
@@ -3,9 +3,19 @@
 import chai = require('chai');
 const assert = chai.assert;
 
+import Web3 = require('web3');
+import HDWalletProvider = require('truffle-hdwallet-provider');
+const config = JSON.parse(fs.readFileSync(path.resolve('../dao/config.json')));
+
+const provider = new HDWalletProvider(config.seed, 'http://localhost:8545', 0, 10);
+const web3 = new Web3(provider);
+
+
 describe('IdentityDAO', () => {
 
-  it('test', () => {
+  it('test thing', async () => {
+    const idRegBuild = fs.readFileSync(path.resolve('../dao/build/contracts/IdentityRegistry.json'));
+    const IdentityRegistry = web3.eth.Contract(idRegBuild.abi, address);
     assert.ok(1==1);
   })
 
