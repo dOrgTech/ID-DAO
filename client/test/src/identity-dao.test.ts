2a5123df494c82c63d71d58bce35e655331ef552
Luis Dominguez <ld@luis.sh>
2019-07-31 20:38:07 -0400

    Added code to grab contract address

--- client/test/src/identity-dao.test.ts
+++ client/test/src/identity-dao.test.ts
@@ -5,21 +5,33 @@ const assert = chai.assert;
 
 import fs = require('fs');
 import path = require('path');
-
 import Web3 = require('web3');
 // @ts-ignore
 import HDWalletProvider = require('truffle-hdwallet-provider');
 const config = JSON.parse(fs.readFileSync(path.resolve('../dao/config.json'), 'utf8'));
 
+
 const provider = new HDWalletProvider(config.seed, 'http://localhost:8545', 0, 10);
 const web3 = new Web3(provider);
 
-
 describe('IdentityDAO', () => {
+
+  let accounts: string[];
+  
+  before(async () => {
+    accounts = await web3.eth.getAccounts();
+  })
 
   it('test thing', async () => {
     const idRegBuild = JSON.parse(fs.readFileSync(path.resolve('../dao/build/contracts/IdentityRegistry.json'), 'utf8'));
-    //const IdentityRegistry = new web3.eth.Contract(idRegBuild.abi, address);
+
+    //How to get address...? Hmmm... we know that our first account (master) will have deployed it, so web3 to look for the thing
+    //Check out: https://web3js.readthedocs.io/en/v1.2.0/web3-eth.html#getpastlogs
+    const idRegLog = await web3.eth.getTransactionFromBlock(3, 0);
+    console.log(idRegLog);
+
+    const IdentityRegistry = new web3.eth.Contract(idRegBuild.abi, idRegLog.to);
+    console.log(await IdentityRegistry.methods.isHuman(accounts[0]).call());
     assert.ok(1==1);
   })
 
--- dao/migrations/1_initial_migration.js
+++ dao/migrations/1_initial_migration.js
@@ -4,10 +4,12 @@ const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer, network, accounts) {
 
+  const master = accounts[0];
+
   //First, deploy Migrations contract
   deployer.deploy(Migrations);
 
   //Deploy IdentityRegistry contract
-  deployer.deploy(IdentityRegistry);
+  deployer.deploy(IdentityRegistry, master);
 
 };
