2e168dc46b04dc987ef494e4c2f54bae2e7bb956
Luis Dominguez <ld@luis.sh>
2019-07-31 21:15:44 -0400

    Think I figured out the hang, needed to close provider

--- client/test/src/identity-dao.test.ts
+++ client/test/src/identity-dao.test.ts
@@ -13,19 +13,19 @@ const config = JSON.parse(fs.readFileSync(path.resolve('../dao/config.json'), 'u
 
 const provider = new HDWalletProvider(config.seed, 'http://localhost:8545', 0, 10);
 const web3 = new Web3(provider);
-/*
+
 describe('IdentityDAO', () => {
 
   let accounts: string[];
   let master: string;
   
-  before(() => {
-//    accounts = await web3.eth.getAccounts();
-//    master = accounts[0];
+  before(async () => {
+    accounts = await web3.eth.getAccounts();
+    master = accounts[0];
   })
 
-  it('test thing', () => {
-    const idRegBuild: any = JSON.parse(fs.readFileSync(path.resolve('../dao/build/contracts/IdentityRegistry.json'), 'utf8'));
+  it('test thing', async () => {
+/*    const idRegBuild: any = JSON.parse(fs.readFileSync(path.resolve('../dao/build/contracts/IdentityRegistry.json'), 'utf8'));
 
     //How to get address...? Hmmm... we know that our first account (master) will have deployed it, so web3 to look for the thing
     //Check out: https://web3js.readthedocs.io/en/v1.2.0/web3-eth.html#getpastlogs
@@ -34,12 +34,12 @@ describe('IdentityDAO', () => {
  
     const IdentityRegistry: any = new web3.eth.Contract(idRegBuild.abi, '0x39aeEe8C35e3BDF394D01e7a9Dab7406C385C61D');
     console.log(await IdentityRegistry.methods.removeSelf().send({ from: master }));
-    assert.ok(1==1);
+    assert.ok(1==1);*/
   })
   
   after(() => {
+    //Close connection
     provider.engine.stop();
-    assert.ok(false)
   })
 
-})*/
+})
