ce05d183274d37ade1605da858f1d98713f6023c
Luis Dominguez <ld@luis.sh>
2019-08-13 18:22:13 -0400

    Adding natspec documentation

--- dao/contracts/Registry.sol
+++ dao/contracts/Registry.sol
@@ -2,6 +2,16 @@ pragma solidity ^0.5.0;
 
 import "./IRegistry.sol";
 
+/**
+ * @title Generic Registry Contract
+ * @author Luis Dominguez <ld@luis.sh>
+ * @dev This is a very basic contract which acts as a simple store for addresses, with CRUD.
+ * Public-facing functions are seperated from internal functions, one dedicated
+ * to logic in exposing functionality, the other for the actual logic.
+ *
+ * Any extra data can be passed as bytes, which allows for complete flexibility.
+ */
+
 contract Registry is IRegistry {
 
     event Add(
@@ -27,6 +37,12 @@ contract Registry is IRegistry {
       Functions of Registry, exposing functionality of internals
     */
 
+    /**
+     * @dev Simple add function, adds an address along with metadata associated with the identity.
+     * 
+     * @param id address Address to add
+     * @param metadata bytes Metadata to link to the address
+     */
     function add(
         address id,
         bytes memory metadata
@@ -38,6 +54,12 @@ contract Registry is IRegistry {
         _remove(id);
     }
 
+    /**
+     * @dev Simple update function, updates an address along with metadata associated with the identity.
+     * 
+     * @param id address Address to update
+     * @param metadata bytes Metadata to link to the address
+     */
     function update(
         address id,
         bytes memory metadata
