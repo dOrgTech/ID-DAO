4782dccb0a39887765b1b71fa1342460b3006b16
Luis Dominguez <ld@luis.sh>
2019-08-13 20:24:56 -0400

    Fixed eslint by adding JSDoc

--- dao/test/solAssert.js
+++ dao/test/solAssert.js
@@ -4,10 +4,22 @@
 
 */
 
+'use strict';
 
+/**
+ * SolAssert static util class
+ */
 class SolAssert {
-  // Simply function to test for Solidity revert errors; optionally takes an "expectedErr" which simply looks for a string within
-  // This function has limits, however; if a function can potentially return two or more reverts, we can't quite test for each of them through expectedErr and apply if/and/or logic
+  /**
+   * Simply function to test for Solidity revert errors; optionally takes an "expectedErr"
+   * which simply looks for a string within
+   *
+   * This function has limits, however; if a function can potentially return two or more reverts,
+   * we can't quite test for each of them through expectedErr and apply if/and/or logic
+   *
+   * @param {function} run - Function to test for Solidity revert errors
+   * @param {string} [expectedErr=null] - Any revert string to expect; throw if not found
+   */
   static async revert(run, expectedErr = null) {
     let err;
     try {
