75d9a6dc6372879c7b53520473f60dc5d2c795d3
Luis Dominguez <ld@luis.sh>
2019-08-15 03:36:04 -0400

    Continuing transition to TypeScript

---
+++ dao/test/src/contracts.ts
@@ -0,0 +1,15 @@
+/**
+
+  General file to use across all test-cases, allows for modular use of artifacts.require()
+
+*/
+
+'use strict';
+
+const Registry = artifacts.require('./Registry.sol');
+const IdentityRegistry = artifacts.require('./IdentityRegistry.sol');
+
+module.exports = {
+  'Registry': Registry,
+  'IdentityRegistry': IdentityRegistry,
+};
