517c1ea8a733df8ef549eefdf0803f2f8e5ebcc0
dOrgJelli <jelli@dorg.tech>
2019-09-26 20:02:05 -0400

    HumanReputationClaim.sol

--- dao/contracts/HumanReputationClaim.sol
+++ dao/contracts/HumanReputationClaim.sol
@@ -1,9 +1,42 @@
 pragma solidity ^0.5.0;
 
-// TODO: RegistryReputationClaim (if user is any of the registries, they can claim rep)
-contract HumanReputationClaim {
-  // TODO:
-  // - universal scheme
-  // - construct w/ address of Human Registry
-  // - register params (avatar=>amount)
+import "@daostack/infra/contracts/Reputation.sol";
+import "@daostack/arc/contracts/universalSchemes/UniversalScheme.sol";
+import "./IdentityRegistry.sol";
+
+contract HumanReputationClaim is UniversalScheme {
+
+  event HumanReputationClaimed(
+    address indexed _avatar,
+    address indexed _human,
+    uint256 _amount
+  );
+
+  // A registry of humans
+  IdentityRegistry public registry;
+
+  constructor(IdentityRegistry _registry) public {
+    registry = _registry;
+  }
+
+  function claim(Avatar _avatar, address _human) public returns(uint256 reputation) {
+    require(registry.isHuman(_human), "human is not registered");
+
+    // Fetch the amount of reputation the human is claiming.
+    // As an optimization, this is simply stored in the controller
+    // as this scheme's params, instead of doing the typical hashing and storing.
+    reputation = uint256(getParametersFromController(_avatar));
+
+    require(reputation > 0, "reputation must be greater than 0");
+    require(
+      _avatar.nativeReputation().balanceOf(_human) == 0,
+      "human has already claimed reputation"
+    );
+    require(
+      ControllerInterface(_avatar.owner()).mintReputation(
+        uint(reputation), _human, address(_avatar)
+      ),
+      "failed to mintReputation"
+    );
+  }
 }
--- dao/truffle-config.js
+++ dao/truffle-config.js
@@ -74,6 +74,7 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`, 0, 10),
       network_id: 4,       // Rinkeby's id
@@ -95,19 +96,14 @@ module.exports = {
   mocha: {
     // timeout: 100000
   },
-
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.4",    // Fetch exact version from solc-bin (default: truffle's version)
+         version: "0.5.11",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+      }
   }
 }
